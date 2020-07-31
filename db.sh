77a4448e42ac914d8cd32a79ca5a0159a5fb534e
bebyx <bebyx@protonmail.com>
2020-07-31 12:46:48 +0300

    Remove unneeded sudo

--- db.sh
+++ db.sh
@@ -19,7 +19,7 @@ GRANT ALL ON dtapi.* TO 'dtapi'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 EOF
 
-sudo sed -i.bak '/bind-address/ s/127.0.0.1/0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i.bak '/bind-address/ s/127.0.0.1/0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 wget -q https://dtapi.if.ua/~yurkovskiy/dtapi_full.sql
 mariadb -u root dtapi < ./dtapi_full.sql
