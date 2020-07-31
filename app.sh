b55fd4ccc821ba3ddff0fef481a3213c2fda9fb7
bebyx <bebyx@protonmail.com>
2020-07-31 18:24:59 +0300

    Add comments to scripts

--- app.sh
+++ app.sh
@@ -2,21 +2,25 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+# Update Debian10 and install needed packages
 apt update && apt upgrade -y
 apt install apache2 -y
 
-#Install required php dependencies:
+# Install required php dependencies
 apt install -y php libapache2-mod-php php-mysql php-mbstring php-gd php-pdo php-xml php-cli php-curl php-http php-json
 
-#Extract app from the shared vagrant folder:
+# Extract web app from the shared vagrant folder
 tar -xjvf /vagrant/dtapiapp.tar.bz2 -C /var/www/
 
+# Set $USER as an owner of web app directory
 chown -R vagrant:vagrant /var/www/dtapi
 
+# Create directories cache, logs and modify permissions to enable them in web app
 mkdir /var/www/dtapi/api/application/cache /var/www/dtapi/api/application/logs
 chmod 733 /var/www/dtapi/api/application/cache
 chmod 722 /var/www/dtapi/api/application/logs
 
+# Set Apache VirtualHost for web app
 cat <<'EOF' > /etc/apache2/sites-available/dtapi.conf
 <VirtualHost *:80>
 #    ServerName your_domain
@@ -31,9 +35,12 @@ cat <<'EOF' > /etc/apache2/sites-available/dtapi.conf
 </Directory>
 EOF
 
+# Enable web app in Apache, disable default Apache website directory
 a2ensite dtapi
 a2dissite 000-default
+# Enable Apache modules (headers and rewrite)
 a2enmod headers
 a2enmod rewrite
 
+# Restart Apache to enable new config
 systemctl restart apache2
--- db.sh
+++ db.sh
@@ -2,9 +2,11 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+# Update Debian10 and install needed packages
 apt update && apt upgrade -y
 apt install mariadb-server wget -y
 
+# Run mysql_secure_installation script
 mariadb -u root <<-EOF
 UPDATE mysql.user SET Password=PASSWORD('') WHERE User='root';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
@@ -13,15 +15,19 @@ DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
 FLUSH PRIVILEGES;
 EOF
 
+# Create web app database and asign user
 mariadb -u root <<-EOF
 CREATE DATABASE dtapi;
 GRANT ALL ON dtapi.* TO 'dtapi'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 EOF
 
+# Let database be publicly reachable
 sed -i.bak '/bind-address/ s/127.0.0.1/0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
+# Import mysql dump into web app database
 wget -q https://dtapi.if.ua/~yurkovskiy/dtapi_full.sql
 mariadb -u root dtapi < ./dtapi_full.sql
 
+# Restart mysql service to enable new mysql config
 systemctl restart mysql
